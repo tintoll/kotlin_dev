0248309e663271149a522dd7570bda6098b3666c
tintoll <belselios@gmail.com>
2024-02-14 22:52:31 +0900

    var, val

--- atomic/src/main/kotlin/chapter1/Vars.kt
+++ atomic/src/main/kotlin/chapter1/Vars.kt
@@ -1,10 +1,15 @@
 package chapter1
 
 fun main() {
-    var whole = 11
-    var fractional = 1.4
-    var words = "Hello, World!"
+    val whole = 11
+    val fractional = 1.4
+    val words = "Hello, World!"
     println(whole)
     println(fractional)
     println(words)
+
+    var sum = 1
+    sum = sum + 2
+    sum += 3
+    println(sum)
 }
