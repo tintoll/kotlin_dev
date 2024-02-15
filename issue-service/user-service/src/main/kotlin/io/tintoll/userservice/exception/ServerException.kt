7bd2407358a9ed2b1f726d46d693cd5fb27fc7e7
tintoll <belselios@gmail.com>
2024-02-15 22:35:39 +0900

    오타 수정

--- issue-service/user-service/src/main/kotlin/io/tintoll/userservice/exception/ServerException.kt
+++ issue-service/user-service/src/main/kotlin/io/tintoll/userservice/exception/ServerException.kt
@@ -6,10 +6,3 @@ sealed class ServerException(
 ) : RuntimeException(message)
 
 
-data class NotFoundException(
-    override val message: String,
-) : ServerException(404, message)
-
-data class UnauthorizedException(
-    override val message: String = "인증 정보가 잘못되었습니다.",
-) : ServerException(401, message)
