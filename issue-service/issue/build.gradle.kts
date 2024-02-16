6f7635ae19c53ed288913c78ae46b96049a45719
tintoll <belselios@gmail.com>
2024-02-17 08:15:57 +0900

    이슈서비스, user서비스 연동

--- issue-service/issue/build.gradle.kts
+++ issue-service/issue/build.gradle.kts
@@ -4,5 +4,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
 
--- issue-service/issue/src/main/kotlin/io/tintoll/issueservice/config/WebConfig.kt
+++ issue-service/issue/src/main/kotlin/io/tintoll/issueservice/config/WebConfig.kt
@@ -1,5 +1,8 @@
 package io.tintoll.issueservice.config
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,11 +10,13 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
 class WebConfig(
-    private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver
+    private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver,
 ) : WebMvcConfigurationSupport() {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
         argumentResolvers.apply {
@@ -21,7 +26,8 @@ class WebConfig(
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(@Value("\${auth.url}") private val authUrl: String) :
+    HandlerMethodArgumentResolver {
     override fun supportsParameter(parameter: MethodParameter): Boolean =
         AuthUser::class.java.isAssignableFrom(parameter.parameterType)
 
@@ -30,12 +36,26 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         mavContainer: ModelAndViewContainer?,
         webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?
     ): Any? {
-        return AuthUser(userId = 1, username = "테스트")
+
+        // 헤더에서 token 가져옴
+        val authHeader = webRequest.getHeader("Authorization")
+
+        // override 하는 메서드가 suspend가 아니기 때문에 runBlocking을 이용하여 코루틴스코프를 만들어줌.
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization", authHeader)
+                .retrieve()
+                .awaitBody<AuthUser>()
+        }
     }
 }
 
 data class AuthUser(
+    @JsonProperty("id")
     val userId: Long,
-    var username: String,
+    val username: String,
+    val email: String,
     val profileUrl: String? = null
 )
